925420684e93175cebc78e2c20f7e98720d42567
baiyelatte <baiyelatte@gmail.com>
2022-10-01 11:03:54 +0800

    first commit

---
+++ src/01组件化开发/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div style={{ width: "100px", height: "100px", backgroundColor: "red" }}>
+        nixx
+      </div>
+    );
+  }
+}
+export default App;
